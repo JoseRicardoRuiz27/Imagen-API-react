7ecd07b000db88e7a8f786e527621d378bf943a1
Jose Ruiz <jruiz793ric@gmail.com>
2024-07-09 22:58:46 -0300

    Cambio el jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react"
 
 const API_RANDOM_IMG = `https://catfact.ninja/fact`
 // const API_IMG_URL = `https://cataas.com/cat/says/${primeraPalabra}?size=50&color=red&json.true`
+const imgCAT = `https://cataas.com`
 
 export default function App(){
     const [fact, setFact] = useState()
+    const [urlImg, setUrlImg] = useState()
     useEffect(() =>{
         fetch(API_RANDOM_IMG).then(respuesta => respuesta.json())
         .then(data => {
@@ -14,14 +16,21 @@ export default function App(){
             //para conseguir las primera palabra del string hacemos un split(` `)[0]
             //para conseguir mas palabras debemos hacer .split(` `).slice(0, 3).join(``)
             const primeraPalabra = fact.split(' ')[0]
-            console.log(primeraPalabra)
+        
+            fetch(`https://cataas.com/cat/says/${primeraPalabra}?fontSize=50&fontColor=red&json=true`)
+            .then(res => res.json())
+            .then(response => {
+                const { url } = response
+                setUrlImg(url)
+            })
         })
     }, [])
 
     return(
-        <>
+        <main>
             <h1>Creacion de App-react con js vanila</h1>
             {fact && <p>{fact}</p>}
-        </>
+            {urlImg && <img src={imgCAT} alt={`Imagen extrida de la primera palabra que es ${fact}`} />}
+        </main>
     )
 }
